daba98ffbd50f4ebdb926c8fc42fe91e061f207c
Eugene R <negfreak@gmail.com>
2018-10-30 22:43:33 +0300

    Funny events

--- Lections HW/HW_5(vectors and ect)/3 string/script.js
+++ Lections HW/HW_5(vectors and ect)/3 string/script.js
@@ -1,4 +1,3 @@
 var exp = /\+375[\s]?\d{2}[\s]?\d{3}[\s\-]?\d{2}[\s\-]?\d{2}$/;
 var str = "+375 44 775-36-10, +375 29 3333333, +375253553535, +375 33 755 25 25"
-
-console.log(str.match(exp));
+if(str.match(exp)){console.log("str.match(exp)");}
